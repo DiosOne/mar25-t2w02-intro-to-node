6a50f0fa89ef12e5541f49df902f47879a819e48
Alex <alex.holder@bigfootds.com>
2025-09-18 20:50:07 +1000

    feat: express basic routes implemented and tested with bruno

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 console.log("Hello, world!");
+
+// Import the package
+const express = require('express');
+// Make an instance of an Express server
+const app = express();
+
+// Enable processing of body content on incoming web requests
+// JSON is raw body JSON content
+app.use(express.json());
+// URL Encoded is typically from a form, like a <form></form>
+app.use(express.urlencoded({extended: true}));
+
+// Grab the port number from environment variables or use a hardcoded fallback value
+const port = 3000;
+
+// Configure the server to respond to GET "/" endpoint requests
+app.get('/', (req, res) => {
+	// Send back some HTML content
+	// res.send('<h1>Hello World!</h1>');
+	res.json({
+		message:"Hello, world!"
+	});
+});
+
+app.post("/", (request, response) => {
+	console.log(request.body);
+	if (!request.body || request.body.message == undefined){
+		return response.status(401).json({error:"Invalid request"})
+	}
+
+
+	let repeatedWord = request.body.message;
+
+	response.json({
+		message: repeatedWord + repeatedWord + repeatedWord
+	});
+});
+
+// Start the server, allow it to listen to web traffic on the specified port 
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
